890dac93d7cf3fbb914e830c2598cc2f066f81dc
Jordy Guzak <jordy.guzak@yunify.nl>
2019-12-04 15:07:58 +0100

    taking canvas scale into account so the transition is consistent on all screen sizes

--- Assets/JOGUI/Scripts/Transitions/SharedElementsTransition.cs
+++ Assets/JOGUI/Scripts/Transitions/SharedElementsTransition.cs
@@ -47,12 +47,13 @@ namespace JOGUI
 
                     if (destinationElement.RectTransform.position != sourceElement.RectTransform.position)
                     {
-                        var deltaPivot = destinationElement.RectTransform.pivot - sourceElement.RectTransform.pivot;
-                        var offsetX = sourceElement.RectTransform.rect.width * deltaPivot.x;
-                        var offsetY = sourceElement.RectTransform.rect.height * deltaPivot.y;
-                        var camera = Camera.main;
-                        var screenPoint = camera.WorldToScreenPoint(sourceElement.RectTransform.position) + new Vector3(offsetX, offsetY, 0);
-                        var startPosition = camera.ScreenToWorldPoint(screenPoint);
+                        //var deltaPivot = destinationElement.RectTransform.pivot - sourceElement.RectTransform.pivot;
+                        //var offsetX = sourceElement.RectTransform.rect.width * deltaPivot.x;
+                        //var offsetY = sourceElement.RectTransform.rect.height * deltaPivot.y;
+                        //var camera = Camera.main;
+                        //var screenPoint = camera.WorldToScreenPoint(sourceElement.RectTransform.position) + new Vector3(offsetX, offsetY, 0);
+                        //var startPosition = camera.ScreenToWorldPoint(screenPoint);
+                        var startPosition = GetStartPosition(sourceElement.RectTransform, destinationElement.RectTransform);
 
                         tweens.Add(new UITween<Vector3>(startPosition, destinationElement.RectTransform.position)
                             .SetDelay(StartDelay)
@@ -65,8 +66,6 @@ namespace JOGUI
 
                     if (destinationElement.RectTransform.rect.size != sourceElement.RectTransform.rect.size)
                     {
-                        var originalPivot = destinationElement.RectTransform.pivot;
-
                         tweens.Add(new UITween<Vector2>(sourceElement.RectTransform.rect.size, destinationElement.RectTransform.rect.size)
                             .SetDelay(StartDelay)
                             .SetDuration(Duration)
@@ -92,6 +91,17 @@ namespace JOGUI
                 .SetDuration(Duration)
                 .SetEaseType(EaseType)
                 .SetOnComplete(_onCompleteCallback);
+        }
+
+        private Vector3 GetStartPosition(RectTransform sourceElement, RectTransform destinationElement)
+        {
+            var canvas = destinationElement.GetComponentInParent<Canvas>().rootCanvas;
+            var deltaPivot = destinationElement.pivot - sourceElement.pivot;
+            var offsetX = sourceElement.rect.width * canvas.scaleFactor * deltaPivot.x;
+            var offsetY = sourceElement.rect.height * canvas.scaleFactor * deltaPivot.y;
+            var camera = Camera.main;
+            var screenPoint = camera.WorldToScreenPoint(sourceElement.position) + new Vector3(offsetX, offsetY, 0);
+            return camera.ScreenToWorldPoint(screenPoint);
         }
     }
 }
